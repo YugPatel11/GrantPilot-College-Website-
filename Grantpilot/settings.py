0022404fc94a5e3a3ea091f1f93c380c58c46a4b
YUG PATEL <work.yugrojivadiya@gmail.com>
2025-11-30 11:06:20 +0530

    host

--- Grantpilot/settings.py
+++ Grantpilot/settings.py
@@ -76,11 +76,12 @@ WSGI_APPLICATION = 'Grantpilot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3"
+    )
 }
 
 
